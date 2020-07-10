abc977a8902c4ad5f27265b7c587f91fa6c6b45b
Rakesh Mane <rakeshmane12345@gmail.com>
2020-07-10 21:45:04 +0530

    Update tmp1.js

--- tmp1.js
+++ tmp1.js
@@ -1,26 +1,4 @@
 
-// ignore below code, it just fixes the broken javascript code to make application usable
-
-window.siteConfig = {
-    logLevel: "WARN",
-    cdnURL: "https://appstream2.us-east-1.aws.amazon.com/",
-    isManagedStack: false,
-    isImageBuilderSession: false,
-    stack: "on-demand-stack",
-    stackOwnerAccountId: "597299136010",
-    region: "us-east-1",
-    userId: "aaaa",
-    feedbackURL: "https://example.com/?on-demand-stack'%2522%253E%253C/script%253E%253Csvg/onload=alert()%253E",
-    authType: "SIGNED_URL",
-    themeInformation: {"titleText":"title","footerLinks":[{"footerLinkURL":"https://example.com/?","displayName":"test"}],"faviconURL":"https://themes.us-east-1.amazonappstream.com/597299136010/theme_on-demand-stack/FAVICON_9f047602-c364-44f5-ab2a-9b4ae18fe948.ico?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20200710T160159Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Credential=AKIAIF6R5WCTTOL2UL7Q%2F20200710%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=ae44e419d1e2609c046084ad4941d9a77a823d4e4fa553233b311085b55c153d","organizationLogoURL":"https://themes.us-east-1.amazonappstream.com/597299136010/theme_on-demand-stack/ORG_LOGO_2ef2e050-63f4-440c-86b1-be4c678f6181.gif?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20200710T160159Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Credential=AKIAIF6R5WCTTOL2UL7Q%2F20200710%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=fdbf55d3132057e3b777ce4fcfb8428362a44be7c7f45089a871b316181c4ec1","stylingProperty":{"spinnerColor":"#404040","bodyTextColor":"#404040","backgroundColor":"#FAF9F7","buttonColor":"#D51900","linkColor":"#D51900"},"stylingName":"Red (#D51900)","themeName":"theme_on-demand-stack"},
-    stackInfo: {"redirectURL":"https://example.com/?on-demand-stack\u0027%2522%253E%253C/script%253E%253Csvg/onload\u003dalert()%253E","embedHostDomains":null,"state":"ACTIVE","accessEndpoints":null,"createdTime":"Jul 8, 2020 5:46:19 PM","applicationSettings":{"settingsGroup":"on-demand-stack","enabled":true,"s3KeyPrefix":null,"s3BucketName":"appstream-app-settings-us-east-1-597299136010-6dbkrz0b"},"themeName":"theme_on-demand-stack","userSettings":[{"action":"CLIPBOARD_COPY_FROM_LOCAL_DEVICE","permission":"ENABLED"},{"action":"CLIPBOARD_COPY_TO_LOCAL_DEVICE","permission":"ENABLED"},{"action":"FILE_UPLOAD","permission":"ENABLED"},{"action":"FILE_DOWNLOAD","permission":"ENABLED"},{"action":"PRINTING_TO_LOCAL_DEVICE","permission":"ENABLED"}],"iamRoleArn":null,"storageConnectors":[{"resourceIdentifier":"appstream2-36fb080bb8-us-east-1-597299136010","connectorType":"S3","domains":null},{"resourceIdentifier":null,"connectorType":"ONE_DRIVE","domains":["https://lol.com/?on-demand-stack\u0027\"\u003e\u003c/script\u003e\u003csvg/onload\u003dalert()\u003e"]},{"resourceIdentifier":null,"connectorType":"GOOGLE_DRIVE","domains":["https://lol.com/?on-demand-stack\u0027\"\u003e\u003c/script\u003e\u003csvg/onload\u003dalert()\u003e"]}],"feedbackURL":"https://example.com/?on-demand-stack\u0027%2522%253E%253C/script%253E%253Csvg/onload\u003dalert()%253E","name":"on-demand-stack","displayName":"on-demand-stack","arn":"arn:aws:appstream:us-east-1:597299136010:stack/on-demand-stack","description":"on-demand-stack\u0027\"\u003e\u003c/script\u003e\u003cimg src onerror\u003dalert()\u003e","awsAccountId":"597299136010","stackErrors":null},
-    useThirdPartySpinner: false,
-    defaultDcvJS: "2020.0-8438"
-  };
-
-// ignore above code, it just fixes the broken javascript code to make application usable
-
-
 
 // Exploit code starts here
 
